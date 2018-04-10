b9bff0491e0dcdbbf6bcdaa2267ef4b139ac3cae
pdkaizer <pdkaizer@gmail.com>
2018-04-10 16:22:29 -0400

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,178 @@
+module.exports = {
+  siteMetadata: {
+    url: 'https://peterkaizer.com',
+    title: 'Peter Kaizer',
+    subtitle: 'Technologist / Designer / Developer',
+    copyright: '©2018 | All rights reserved.',
+    disqusShortname: 'peterkaizer',
+    menu: [
+      {
+        label: 'Articles',
+        path: '/'
+      },
+      {
+        label: 'About me',
+        path: '/about/'
+      },
+      {
+        label: 'Work',
+        path: '/work/'
+      },
+      {
+        label: 'Photos',
+        path: '/photos/'
+      },
+      {
+        label: 'Contact me',
+        path: '/contact/'
+      }
+    ],
+    author: {
+      name: 'Peter Kaizer',
+      email: '#',
+      telegram: '#',
+      twitter: '#',
+      github: '#',
+      rss: '#',
+      vk: '#'
+    }
+  },
+  plugins: [
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages'
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-feed',
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                site_url: url
+                title
+                description: subtitle
+              }
+            }
+          }
+        `,
+        feeds: [
+          {
+            serialize: ({ query: { site, allMarkdownRemark } }) => (
+              allMarkdownRemark.edges.map(edge =>
+                Object.assign({}, edge.node.frontmatter, {
+                  description: edge.node.frontmatter.description,
+                  date: edge.node.frontmatter.date,
+                  url: site.siteMetadata.site_url + edge.node.fields.slug,
+                  guid: site.siteMetadata.site_url + edge.node.fields.slug,
+                  custom_elements: [{ 'content:encoded': edge.node.html }]
+                }))
+            ),
+            query: `
+              {
+                allMarkdownRemark(
+                  limit: 1000,
+                  sort: { order: DESC, fields: [frontmatter___date] },
+                  filter: { frontmatter: { layout: { eq: "post" }, draft: { ne: true } } }
+                ) {
+                  edges {
+                    node {
+                      html
+                      fields {
+                        slug
+                      }
+                      frontmatter {
+                        title
+                        date
+                        layout
+                        draft
+                        description
+                        link
+                      }
+                    }
+                  }
+                }
+              }
+            `,
+            output: '/rss.xml'
+          }
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 960
+            }
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+            options: { wrapperStyle: 'margin-bottom: 1.0725rem' }
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants'
+        ]
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp',
+    {
+      resolve: 'gatsby-plugin-google-analytics',
+      options: { trackingId: 'UA-145398-25' }
+    },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+            `Source Sans Pro\:300,300i,400,700,700i`,
+            `PT+Serif\:400,700`
+            ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-sitemap',
+      options: {
+        query: `
+            {
+              site {
+                siteMetadata {
+                  url
+                }
+              }
+              allSitePage(
+                filter: {
+                  path: { regex: "/^(?!/404/|/404.html|/dev-404-page/)/" }
+                }
+              ) {
+                edges {
+                  node {
+                    path
+                  }
+                }
+              }
+          }`,
+        output: '/sitemap.xml',
+        serialize: ({ site, allSitePage }) =>
+          allSitePage.edges.map((edge) => {
+            return {
+              url: site.siteMetadata.url + edge.node.path,
+              changefreq: 'daily',
+              priority: 0.7
+            };
+          })
+      }
+    },
+    'gatsby-plugin-offline',
+    'gatsby-plugin-catch-links',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-postcss-sass'
+  ]
+};
